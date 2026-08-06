4ed09979068b37b68f2b6ef2e26cca57d96e2ac4
guilie360 <arquillem@gmail.com>
2026-08-05 20:32:23 -0500

    Revert "fix(scenes): vertical reorder line in group float panel (ws7666)"
    This reverts commit e46873de6ee399500f32d131162b5219e34b08b8.

--- js/builder/quotation/quotation-editor.js
+++ js/builder/quotation/quotation-editor.js
@@ -6128,22 +6128,6 @@ var QuotationEditor = (function () {
     markDirtyLocal();
   }
 
-  function sceneReorderUsesVerticalAxis(wrap) {
-    if (!wrap || !wrap.classList) return false;
-    if (wrap.classList.contains('is-in-group') || wrap.classList.contains('is-in-panel')) {
-      return true;
-    }
-    return !!(wrap.closest && wrap.closest('.qe-scenes-group-float__scroll'));
-  }
-
-  function sceneReorderPlaceAfter(wrap, e) {
-    var rect = wrap.getBoundingClientRect();
-    if (sceneReorderUsesVerticalAxis(wrap)) {
-      return e.clientY > rect.top + rect.height / 2;
-    }
-    return e.clientX > rect.left + rect.width / 2;
-  }
-
   function clearSceneReorderIndicators(root) {
     var scope = root || document;
     scope.querySelectorAll(
@@ -6261,7 +6245,8 @@ var QuotationEditor = (function () {
         e.stopPropagation();
         if (e.dataTransfer) e.dataTransfer.dropEffect = 'move';
         clearReorderIndicators();
-        var after = sceneReorderPlaceAfter(wrap, e);
+        var rect = wrap.getBoundingClientRect();
+        var after = e.clientX > rect.left + rect.width / 2;
         wrap.classList.add(after ? 'is-scene-reorder-after' : 'is-scene-reorder-before');
         dropHint = { toId: toId, after: after };
       });
@@ -6283,7 +6268,8 @@ var QuotationEditor = (function () {
         var to = (dropHint && dropHint.toId) || wrap.getAttribute('data-qe-scene-drop');
         var after = !!(dropHint && dropHint.after);
         if (!dropHint) {
-          after = sceneReorderPlaceAfter(wrap, e);
+          var rect = wrap.getBoundingClientRect();
+          after = e.clientX > rect.left + rect.width / 2;
         }
         dropHint = null;
         if (from && to && from !== to && from !== 'qe-scene') {
